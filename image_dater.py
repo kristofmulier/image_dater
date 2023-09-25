0acb120f5400fbe27c9d765fc935adc25e17fa41
Kristof Mulier <kristof.mulier@telenet.be>
2023-09-25 16:43:41 +0200

    Update image_dater.py

--- image_dater.py
+++ image_dater.py
@@ -2,14 +2,14 @@ from typing import *
 import subprocess, re, os, datetime, argparse, sys, traceback
 
 # Keep a pattern to extract dates from the metadata.
-p = re.compile(r'(20\d\d)(\d\d)(\d\d)(\d\d)(\d\d)(\d\d)\.(\d*)')
+p1 = re.compile(r'(20\d\d)(\d\d)(\d\d)(\d\d)(\d\d)(\d\d)\.(\d*)')
+
+# The following pattern matches with filenames that are already converted by this script.
+p2 = re.compile(r'(20\d\d)(\d\d)(\d\d)-(\d\d)(\d\d)(\d\d)-(\d\d)')
 
 # Keep a dictionary with all the full filepaths as keys (guaranteed to be unique) and dates as the
 # values. The dates are already converted into strings here.
 filepath_dict:Dict[str, str] = {}
-
-# Keep a flipped dictionary, with the dates as keys and the original names as values.
-flipped_dict:Dict[str, List[str]] = {}
 
 def show_help() -> None:
     '''
@@ -27,9 +27,25 @@ def get_image_dates(filepath) -> List[datetime.datetime]:
     '''
     Extract dates from the given image.
     '''
-    filepath = filepath.replace('/', '\\')
-    filepath = filepath.replace('\\', '\\\\')
-    cmd = f'cmd.exe /c wmic datafile "{filepath}" list full'
+    # First check if the date can be extracted from the name. If so, just return that date in a one-
+    # element list.
+    filename = filepath.split('/')[-1]
+    m = p2.search(filename)
+    if m is not None:
+        return [
+            datetime.datetime(
+                year   = int(m.group(1)),
+                month  = int(m.group(2)),
+                day    = int(m.group(3)),
+                hour   = int(m.group(4)),
+                minute = int(m.group(5)),
+                second = int(m.group(6)),
+            ),
+        ]
+    # Extract the date(s) from the metadata, then return them in a sorted list (eldest ones first).
+    filepath_bsl = filepath.replace('/', '\\')
+    filepath_bsl = filepath_bsl.replace('\\', '\\\\')
+    cmd = f'cmd.exe /c wmic datafile "{filepath_bsl}" list full'
     output = subprocess.Popen(
         cmd,
         stdout=subprocess.PIPE,
@@ -38,7 +54,7 @@ def get_image_dates(filepath) -> List[datetime.datetime]:
     ).communicate()[0]
     output_utf = output.decode('utf-8', errors='ignore')
     dates = []
-    for m in p.finditer(output_utf):
+    for m in p1.finditer(output_utf):
         try:
             dates.append(
                 datetime.datetime(
@@ -69,15 +85,18 @@ def parse_images(dirpath):
     '''
     Parse the given folder for images and date them.
     '''
+    # First fill the dictionary 'filepath_dict' with filepath-date pairs.
     for root, dirs, files in os.walk(dirpath):
         for f in files:
-            if not f.lower().endswith(('.heic', '.mov', '.jpeg', '.jpg', '.mp4')):
+            if not f.lower().endswith(('.heic', '.mov', '.jpeg', '.jpg', '.mp4', '.png')):
                 continue
             filepath = os.path.join(root, f).replace('\\', '/')
+            filename = filepath.split('/')[-1]
             try:
                 date_str = date_to_str(get_image_dates(filepath)[0])
             except:
                 print(f'Cannot parse file: {filepath}')
+                sys.stdout.flush()
                 date_str = None
                 traceback.print_exc()
             if date_str is not None:
@@ -85,19 +104,35 @@ def parse_images(dirpath):
             continue
         continue
 
+    # Now loop through the 'filepath_dict' and eliminate all the files from the dictionary that are
+    # clearly already parsed in a previous run of this script. They don't need to be renamed once
+    # again, obviously.
+    for filepath in list(filepath_dict.keys()):
+        filename = filepath.split('/')[-1]
+        m = p2.search(filename)
+        if m is not None:
+            del filepath_dict[filepath]
+            print(f'SKIP: {filepath}')
+            sys.stdout.flush()
+        continue
+
+    # Loop again through the 'filepath_dict' and rename the files according to their dates. 
     for filepath in filepath_dict.keys():
         date_str = filepath_dict[filepath]
-        cntr = 0
-        if date_str not in flipped_dict.keys():
-            flipped_dict[date_str] = [filepath, ]
-        else:
-            cntr = len(flipped_dict[date_str])
-            flipped_dict[date_str].append(filepath)
         src_filepath = filepath
-        dst_filepath = os.path.join(
-            os.path.dirname(filepath),
-            f'{date_str}-{cntr:02d}',
-        ).replace('\\', '/') + os.path.splitext(filepath)[1]
+        cntr = 0
+        dst_filepath = None
+        while True:
+            dst_filepath = os.path.join(
+                os.path.dirname(filepath),
+                f'{date_str}-{cntr:02d}',
+            ).replace('\\', '/') + os.path.splitext(filepath)[1]
+            if os.path.exists(dst_filepath):
+                cntr += 1
+                continue
+            break
+        assert dst_filepath is not None
+        assert not os.path.exists(dst_filepath)
         print(f'RENAME: {src_filepath} => {dst_filepath}')
         sys.stdout.flush()
         os.rename(src_filepath, dst_filepath)
