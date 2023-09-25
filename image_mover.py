cad8b7b586ed5d086ebc921bc1cae4df8ffd69d5
Kristof Mulier <kristof.mulier@telenet.be>
2023-09-25 18:22:43 +0200

    Create image_mover.py

---
+++ image_mover.py
@@ -0,0 +1,128 @@
+from typing import *
+import subprocess, re, os, datetime, argparse, sys, traceback, shutil
+q = "'"
+basefolder = 'C:/Backup'
+
+# The following pattern matches with filenames that are already converted by the 'image_dater.py'
+# script.
+p = re.compile(r'(20\d\d)(\d\d)(\d\d)-(\d\d)(\d\d)(\d\d)-(\d+)')
+
+def show_help() -> None:
+    '''
+    Print help info and quit.
+    '''
+    print('Move images based on their date.')
+
+def date_to_str(date_obj:datetime.datetime) -> str:
+    '''
+    Convert the date object to a string like:
+    '20230601-184100'
+    '''
+    return date_obj.strftime('%Y%m%d-%H%M%S')
+
+def parse_images(dirpath):
+    '''
+    Parse the given folder for images and move them.
+    '''
+    # First fill a list with all the files that are already converted by the 'image_dater.py'
+    # script.
+    filepath_list = []
+    for root, dirs, files in os.walk(dirpath):
+        for f in files:
+            if not f.lower().endswith(('.heic', '.mov', '.jpeg', '.jpg', '.mp4', '.png')):
+                continue
+            filepath = os.path.join(root, f).replace('\\', '/')
+            filename = filepath.split('/')[-1]
+            m = p.search(filename)
+            if m is None:
+                continue
+            filepath_list.append(filepath)
+            continue
+        continue
+
+    # Now loop over the list and move the files
+    for filepath in filepath_list:
+        assert os.path.isfile(filepath)
+        filename = filepath.split('/')[-1]
+        m = p.search(filename)
+        assert m is not None
+        year_str   = m.group(1)
+        month_str  = m.group(2)
+        day_str    = m.group(3)
+        hour_str   = m.group(4)
+        minute_str = m.group(5)
+        second_str = m.group(6)
+        cntr_str   = m.group(7)
+        filedate = datetime.datetime(
+            year   = int(year_str),
+            month  = int(month_str),
+            day    = int(day_str),
+            hour   = int(hour_str),
+            minute = int(minute_str),
+            second = int(second_str),
+        )
+
+        # Compute ideal destination path
+        src_filepath   = filepath
+        dst_filepath   = f'{basefolder}/Pictures_{year_str}/{month_str}_{year_str}/{filename}'
+        dst_folderpath = os.path.dirname(dst_filepath).replace('\\', '/')
+
+        # Source and ideal destination paths are already identical. No need to move anything.
+        # Continue to next file.
+        if src_filepath == dst_filepath:
+            print(f'GOOD: {src_filepath}')
+            continue
+
+        # Maybe another file is already at the ideal destination path. Increase a counter in the
+        # filename before moving this file.
+        if os.path.isfile(dst_filepath):
+            cntr = int(cntr_str)
+            while True:
+                dst_filepath = str(
+                    f'{dst_folderpath}/'
+                    f'{date_to_str(filedate)}-{cntr:02d}.'
+                    f'{os.path.splitext(filename)}'
+                )
+                if os.path.exists(dst_filepath):
+                    cntr += 1
+                    continue
+                break
+
+        # At this point, we know that the current file must be moved to 'dst_filepath' and the
+        # destination doesn't exist yet (the cntr has been taken care of). Move the file now.
+        assert not os.path.exists(dst_filepath)
+        if not os.path.isdir(dst_folderpath):
+            print(f'CREATE: {dst_folderpath}')
+            os.makedirs(dst_folderpath)
+        print(f'MOVE: {src_filepath} => {dst_filepath}')
+        sys.stdout.flush()
+        shutil.move(src_filepath, dst_filepath)
+        continue
+    return
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description = 'Move images based on their date.',
+        add_help    = False,
+    )
+    parser.add_argument('-h', '--help', action='store_true')
+    parser.add_argument('-d', '--directory', action='store')
+    args = parser.parse_args()
+    if args.help:
+        show_help()
+    if args.directory is not None:
+        if not os.path.isdir(args.directory):
+            print(f'ERROR: Cannot find directory: {q}{args.directory}{q}')
+            sys.exit(0)
+        user_input = input(
+            f'Process the pictures from this folder:\n'
+            f'{q}{args.directory}{q}\n'
+            f'and store them at this location:\n'
+            f'{q}{basefolder}{q}\n'
+            f'Proceed? [yes|no]\n'
+        )
+        if user_input.lower() in ('y', 'yes'):
+            parse_images(args.directory)
+
+    print('\nQuit image mover tool\n')
+    sys.exit(0)
