8c0baff6c6dda877d1f33646549a7e16f3bcf321
Kristof Mulier <kristof.mulier@telenet.be>
2024-04-14 11:54:20 +0200

    Update image dater tool

---
+++ exif_parser.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# This script extracts the date the photo was taken from the exif data of an image file. It uses the
+# exiftool command-line tool to retrieve the exif data and parses it to extract the date. The
+# extracted date is returned as a datetime object.
+# If the date cannot be extracted or the file does not exist, None is returned. This script can be
+# run from the command line with the -f or --file option to specify the image file. If no file is
+# specified, the script exits without doing anything.
+#
+# Example usage: python exif_parser.py -f image.jpg
+#
+# $ sudo apt install libimage-exiftool-perl
+# $ sudo apt install python-is-python3
+
+from typing import *
+import subprocess, re, os, datetime, traceback, argparse, sys
+
+p1 = re.compile(
+    r"^([^:]+):\s+(\d{4}:\d{2}:\d{2} \d{2}:\d{2}:\d{2}(?:\.\d+)?(?:[+-]\d{2}:\d{2})?Z?)",
+    re.MULTILINE,
+)
+
+p2 = re.compile(
+    r"(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})(\.\d+)?([+-]\d{2}:\d{2}|Z)?",
+)
+
+def get_photo_taken_date(filepath: str, verbose: bool = False) -> Optional[datetime.datetime]:
+    '''
+    Extract the date the photo was taken from the exif data.
+    '''
+    date_dict = parse_exif_data(filepath, verbose)
+    if not date_dict:
+        return None
+    # Ordered list of exif fields by their priority
+    fields_priority = [
+        'Date/Time Original',
+        'Creation Date',
+        'Media Create Date',
+        'Track Create Date',
+        'Create Date',
+        'Date Created',
+        'Modify Date',
+        'File Modification Date/Time'
+    ]
+    for field in fields_priority:
+        if field in date_dict:
+            if verbose:
+                print(f"Chosen field: '{field}'")
+            return date_dict[field]
+    return None
+
+def parse_exif_data(filepath:str, verbose:bool=False) -> Dict[str, datetime.datetime]:
+    '''
+    Extract exif data from an image file.
+    '''
+    cmd = f'exiftool "{filepath}"'
+    output = subprocess.Popen(
+        cmd,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        shell=True,
+    ).communicate()[0]
+    output_utf = output.decode('utf-8', errors='ignore')
+    if verbose:
+        print('-' * 80)
+        print(output_utf)
+        print('-' * 80)
+    date_dict = {}
+    for m in p1.finditer(output_utf):
+        label = m.group(1)
+        date_str = m.group(2)
+        normalized_date_str = to_isoformat_with_microseconds(date_str)
+        try:
+            date_obj = datetime.datetime.fromisoformat(normalized_date_str)
+            date_dict[label.strip()] = date_obj
+        except ValueError:
+            pass
+        continue
+    return date_dict
+
+def to_isoformat_with_microseconds(date_str:str) -> str:
+    '''
+    Normalize a date string to ISO format with microseconds precision. For example:
+        - '2024:04:01 05:36:42+02:00'      -> '2024-04-01 05:36:42.000000+02:00'
+        - '2024:03:31 22:38:17'            -> '2024-03-31 22:38:17.000000+00:00'
+        - '2022:01:01 00:00:00'            -> '2022-01-01 00:00:00.000000+00:00'
+        - '2024:03:31 22:38:17.767+08:00'  -> '2024-03-31 22:38:17.767000+08:00'
+        - '2024:03:31 14:38:15.29Z'        -> '2024-03-31 14:38:15.290000+00:00'
+    '''
+    #$ Replace colon separators in the date part to dashes if necessary
+    date_str = re.sub(r'^(\d{4}):(\d{2}):(\d{2})', r'\1-\2-\3', date_str.strip())
+    #$ Regex to extract the base datetime, the fractional part, and the timezone
+    match = p2.match(date_str)
+    if not match:
+        raise ValueError("Invalid date format")
+    base_datetime, fractional_seconds, timezone = match.groups()
+    #$ Normalize the fractional seconds to microseconds precision
+    if fractional_seconds:
+        # Remove the decimal point, extend or trim to 6 digits (microseconds)
+        fractional_seconds = (fractional_seconds[1:] + '000000')[:6]
+        fractional_seconds = '.' + fractional_seconds
+    else:
+        # If no fractional part was found, append zero microseconds
+        fractional_seconds = '.000000'
+    #$ Normalize timezone
+    if timezone == 'Z':
+        timezone = '+00:00'
+    elif not timezone:
+        # If no timezone provided, assume UTC
+        timezone = '+00:00'
+    #$ Reassemble the date string into ISO format
+    return f"{base_datetime}{fractional_seconds}{timezone}"
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description = 'Rename images based on their date.',
+        add_help    = False,
+    )
+    parser.add_argument('-h', '--help',      action='store_true')
+    parser.add_argument('-f', '--file',      action='store')
+    parser.add_argument('-v', '--verbose',   action='store_true')
+    args = parser.parse_args()
+    if args.help:
+        print('\nQuit exif parser tool\n')
+        sys.exit(0)
+
+    if args.file:
+        if not os.path.isfile(args.file):
+            print(f"ERROR: Cannot find file: '{args.file}'")
+            sys.exit(1)
+        if args.verbose:
+            print('')
+            print(f"Parsing exif data from: '{args.file}'")
+            date_obj = get_photo_taken_date(args.file, args.verbose)
+            print(f"Date taken:   '{date_obj}'")
+        else:
+            date_obj = get_photo_taken_date(args.file, args.verbose)
+            print(f"Date taken: {date_obj}")
+        sys.exit(0)
+    
+    print('\nQuit exif parser tool\n')
+    sys.exit(0)
--- image_dater.py
+++ image_dater.py
@@ -1,22 +1,124 @@
 from typing import *
 import subprocess, re, os, datetime, argparse, sys, traceback
-q = "'"
-
-# Keep a pattern to extract dates from the metadata.
-p1 = re.compile(r'(20\d\d)(\d\d)(\d\d)(\d\d)(\d\d)(\d\d)\.(\d*)')
-
-# The following pattern matches with filenames that are already converted by this script.
-p2 = re.compile(r'(20\d\d)(\d\d)(\d\d)-(\d\d)(\d\d)(\d\d)-(\d+)')
-
-# Keep a dictionary with all the full filepaths as keys (guaranteed to be unique) and dates as the
-# values. The dates are already converted into strings here.
-filepath_dict:Dict[str, str] = {}
+import exif_parser
 
 def show_help() -> None:
     '''
     Print help info and quit.
     '''
     print('Rename images based on their date.')
+    print('Usage:')
+    print('    python image_dater.py -h')
+    print('    python image_dater.py -d <directory>')
+    print('    python image_dater.py -f <file> [-v]')
+    print('Options:')
+    print('    -h, --help                  Show this help message and exit.')
+    print('')
+    print('    -d, --directory <directory> All images in the directory and its subdirectories will')
+    print('                                be parsed and renamed according to their date.')
+    print('')
+    print('    -f, --file <file>           Specify a file to investigate. Show its date taken.')
+    print('')
+    print('    -v, --verbose               Print more information.')
+    print('')
+    print('    -n, --dry-run               Do not rename files. Only show what would be done.')
+    print('')
+    return
+
+def parse_and_rename_images(dirpath:str, dry_run:bool=False, verbose:bool=False) -> None:
+    '''
+    Parse the given folder and its subfolders for images and date them. Then rename the images based
+    on those dates, but keep the file extension intact.
+    '''
+    # First, count the number of files in the directory and its subdirectories. This will be used to
+    # draw a progress bar.
+    file_count:int = 0
+    for root, dirs, files in os.walk(dirpath):
+        file_count += len(files)
+
+    # Create a dictionary to store the filepath-date pairs. Filepaths are guaranteed to be unique.
+    # The dates are stored in this dictionary as strings. To fill this dictionary, the exif data of
+    # each image file is parsed. That requires some time - so we draw a progress bar.
+    filepath_date_dict:Dict[str, str] = {}
+    file_cntr:int = 0
+    for root, dirs, files in os.walk(dirpath):
+        for f in files:
+            file_cntr += 1
+            draw_progress_bar(
+                total    = file_count,
+                progress = file_cntr,
+                label    = 'Parse files ',
+            )
+            if not f.lower().endswith(('.heic', '.mov', '.jpeg', '.jpg', '.mp4', '.png')):
+                continue
+            filepath = os.path.join(root, f).replace('\\', '/')
+            filename = filepath.split('/')[-1]
+            date_str:Optional[str] = None
+            try:
+                date_str = date_to_str(exif_parser.get_photo_taken_date(filepath))
+            except:
+                print(f'Cannot parse file: {filepath}')
+                sys.stdout.flush()
+                date_str = None
+                # traceback.print_exc()
+            if date_str is not None:
+                filepath_date_dict[filepath] = date_str
+            continue
+        continue
+    print('')
+
+    # Loop through the 'filepath_date_dict' and rename the files according to their dates. The re-
+    # naming is done in the same directory as the original file. The computed filename results from
+    # appending the date string to a counter. If the computed filename already exists, the counter
+    # is incremented until a unique filename is found.
+    # However, if the computed filename equals the current one - then the file is already named
+    # correctly. In this case, the file is not renamed.
+    file_count = len(filepath_date_dict)
+    file_cntr = 0
+    for filepath in filepath_date_dict.keys():
+        file_cntr += 1
+        date_str = filepath_date_dict[filepath]
+        src_filepath = filepath
+        cntr = 0
+        dst_filepath = None
+        while True:
+            dst_filepath = os.path.join(
+                os.path.dirname(filepath),
+                f'{date_str}-{cntr:03d}',
+            ).replace('\\', '/') + os.path.splitext(filepath)[1]
+            if not os.path.exists(dst_filepath):
+                break
+            if dst_filepath == src_filepath:
+                break
+            cntr += 1
+            continue
+        assert dst_filepath is not None
+        if dst_filepath == src_filepath:
+            if verbose:
+                print(f'GOOD: {src_filepath}')
+                sys.stdout.flush()
+            else:
+                draw_progress_bar(
+                    total    = file_count,
+                    progress = file_cntr,
+                    label    = 'Rename files',
+                )
+            continue
+        assert not os.path.exists(dst_filepath)
+        if verbose:
+            print(f'RENAME: {src_filepath} => {dst_filepath}')
+            sys.stdout.flush()
+        else:
+            draw_progress_bar(
+                total    = file_count,
+                progress = file_cntr,
+                label    = 'Rename files',
+            )
+        if not dry_run:
+            os.rename(src_filepath, dst_filepath)
+        continue
+    print('')
+    return
 
 def date_to_str(date_obj:datetime.datetime) -> str:
     '''
@@ -24,120 +126,31 @@ def date_to_str(date_obj:datetime.datetime) -> str:
     '''
     return date_obj.strftime('%Y%m%d-%H%M%S')
 
-def get_image_dates(filepath) -> List[datetime.datetime]:
-    '''
-    Extract dates from the given image.
-    '''
-    # First check if the date can be extracted from the name. If so, just return that date in a one-
-    # element list.
-    filename = filepath.split('/')[-1]
-    m = p2.search(filename)
-    if m is not None:
-        return [
-            datetime.datetime(
-                year   = int(m.group(1)),
-                month  = int(m.group(2)),
-                day    = int(m.group(3)),
-                hour   = int(m.group(4)),
-                minute = int(m.group(5)),
-                second = int(m.group(6)),
-            ),
-        ]
-    # Extract the date(s) from the metadata, then return them in a sorted list (eldest ones first).
-    filepath_bsl = filepath.replace('/', '\\')
-    filepath_bsl = filepath_bsl.replace('\\', '\\\\')
-    cmd = f'cmd.exe /c wmic datafile "{filepath_bsl}" list full'
-    output = subprocess.Popen(
-        cmd,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        shell=True,
-    ).communicate()[0]
-    output_utf = output.decode('utf-8', errors='ignore')
-    dates = []
-    for m in p1.finditer(output_utf):
-        try:
-            dates.append(
-                datetime.datetime(
-                    year        = int(m.group(1)),
-                    month       = int(m.group(2)),
-                    day         = int(m.group(3)),
-                    hour        = int(m.group(4)),
-                    minute      = int(m.group(5)),
-                    second      = int(m.group(6)),
-                    microsecond = int(m.group(7)),
-                )
-            )
-        except ValueError:
-            dates.append(
-                datetime.datetime(
-                    year        = int(m.group(1)),
-                    month       = int(m.group(2)),
-                    day         = int(m.group(3)),
-                    hour        = int(m.group(4)),
-                    minute      = int(m.group(5)),
-                    second      = int(m.group(6)),
-                    microsecond = 0,
-                )
-            )
-    return sorted(dates)
+progbar_percent:Optional[int] = None
+def draw_progress_bar(total:int, progress:int, label:str):
+    """
+    Draws a progress bar to the console.
 
-def parse_images(dirpath):
-    '''
-    Parse the given folder for images and date them.
-    '''
-    # First fill the dictionary 'filepath_dict' with filepath-date pairs.
-    for root, dirs, files in os.walk(dirpath):
-        for f in files:
-            if not f.lower().endswith(('.heic', '.mov', '.jpeg', '.jpg', '.mp4', '.png')):
-                continue
-            filepath = os.path.join(root, f).replace('\\', '/')
-            filename = filepath.split('/')[-1]
-            try:
-                date_str = date_to_str(get_image_dates(filepath)[0])
-            except:
-                print(f'Cannot parse file: {filepath}')
-                sys.stdout.flush()
-                date_str = None
-                traceback.print_exc()
-            if date_str is not None:
-                filepath_dict[filepath] = date_str
-            continue
-        continue
-
-    # Now loop through the 'filepath_dict' and eliminate all the files from the dictionary that are
-    # clearly already parsed in a previous run of this script. They don't need to be renamed once
-    # again, obviously.
-    for filepath in list(filepath_dict.keys()):
-        filename = filepath.split('/')[-1]
-        m = p2.search(filename)
-        if m is not None:
-            del filepath_dict[filepath]
-            print(f'SKIP: {filepath}')
-            sys.stdout.flush()
-        continue
-
-    # Loop again through the 'filepath_dict' and rename the files according to their dates. 
-    for filepath in filepath_dict.keys():
-        date_str = filepath_dict[filepath]
-        src_filepath = filepath
-        cntr = 0
-        dst_filepath = None
-        while True:
-            dst_filepath = os.path.join(
-                os.path.dirname(filepath),
-                f'{date_str}-{cntr:02d}',
-            ).replace('\\', '/') + os.path.splitext(filepath)[1]
-            if os.path.exists(dst_filepath):
-                cntr += 1
-                continue
-            break
-        assert dst_filepath is not None
-        assert not os.path.exists(dst_filepath)
-        print(f'RENAME: {src_filepath} => {dst_filepath}')
-        sys.stdout.flush()
-        os.rename(src_filepath, dst_filepath)
-        continue
+    Args:
+        total (int): The total count of the operation (i.e., 100% completion).
+        progress (int): The current progress count.
+        bar_length (int): Length of the progress bar in characters. Default is 50.
+    """
+    bar_length:int=50
+    global progbar_percent
+    # Calculate percentage completion
+    percent = int((progress / total) * 100)
+    if percent == progbar_percent:
+        # No need to draw again
+        return
+    # Calculate the number of filled positions
+    filled_length = int(bar_length * progress // total)
+    # Create the bar string
+    bar = '█' * filled_length + '-' * (bar_length - filled_length)
+    # Print the progress bar with carriage return
+    sys.stdout.write(f'\r{label}: |{bar}| {percent}%')
+    sys.stdout.flush()
+    progbar_percent = percent
     return
 
 if __name__ == '__main__':
@@ -145,16 +158,57 @@ if __name__ == '__main__':
         description = 'Rename images based on their date.',
         add_help    = False,
     )
-    parser.add_argument('-h', '--help', action='store_true')
+    parser.add_argument('-h', '--help',      action='store_true')
     parser.add_argument('-d', '--directory', action='store')
+    parser.add_argument('-f', '--file',      action='store')
+    parser.add_argument('-v', '--verbose',   action='store_true')
+    parser.add_argument('-n', '--dry-run',   action='store_true')
     args = parser.parse_args()
+
+    #& Sanitize input
+    if not args.help and not args.directory and not args.file:
+            print('ERROR: No action specified.')
+            show_help()
+            print('\nQuit image dater tool\n')
+            sys.exit(0)
+    if args.directory and args.file:
+        print('ERROR: Cannot use both -d and -f options at the same time.')
+        print('\nQuit image dater tool\n')
+        sys.exit(0)
+
+    #& Show help and quit
     if args.help:
         show_help()
-    if args.directory is not None:
-        if not os.path.isdir(args.directory):
-            print(f'ERROR: Cannot find directory: {q}{args.directory}{q}')
-            sys.exit(0)
-        parse_images(args.directory)
+        print('\nQuit image dater tool\n')
+        sys.exit(0)
 
+    #& Parse directory
+    if args.directory:
+        if not os.path.isdir(args.directory):
+            print(f"ERROR: Cannot find directory: '{args.directory}'")
+            print('\nQuit image dater tool\n')
+            sys.exit(1)
+        parse_and_rename_images(args.directory, args.dry_run, args.verbose)
+        print('\nQuit image dater tool\n')
+        sys.exit(0)
+
+    #& Inspect file
+    if args.file:
+        if not os.path.isfile(args.file):
+            print(f"ERROR: Cannot find file: '{args.file}'")
+            print('\nQuit image dater tool\n')
+            sys.exit(1)
+        if args.verbose:
+            print('')
+            print(f"Parsing exif data from: '{args.file}'")
+            _date_obj = exif_parser.get_photo_taken_date(args.file, args.verbose)
+            print(f"Date taken:   '{_date_obj}'")
+        else:
+            _date_obj = exif_parser.get_photo_taken_date(args.file, args.verbose)
+            print(f"Date taken: {_date_obj}")
+        print('\nQuit image dater tool\n')
+        sys.exit(0)
+    
+    show_help()
     print('\nQuit image dater tool\n')
     sys.exit(0)
